b1cea0cc849d57ba765c04ab7bcdaf799b7ad084
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-10-20 00:08:38 +0200

    user store testing

--- internal/stores/users.go
+++ internal/stores/users.go
@@ -6,11 +6,16 @@ import (
 	"time"
 )
 
+var (
+	ErrUserNotFound     = errors.New("user not found")
+	ErrUserAlreadyExist = errors.New("user already exist")
+)
+
 type User struct {
 	ID             uuid.UUID
 	Email          string
 	EmailConfirmed *time.Time
-	Password       string
+	PasswordHash   string
 }
 
 type UserStore struct {
@@ -27,9 +32,36 @@ func NewUserStore() *UserStore {
 }
 
 func (u *UserStore) Create(user User) (*User, error) {
-	return nil, errors.New("not implemented")
+	if _, ok := u.items[user.ID]; ok {
+		return nil, ErrUserAlreadyExist
+	}
+
+	u.items[user.ID] = user
+	localUser := u.items[user.ID]
+
+	return &localUser, nil
 }
 
 func (u *UserStore) Get(id uuid.UUID) (*User, error) {
-	return nil, errors.New("not implemented")
+	if localUser, ok := u.items[id]; ok {
+		return &localUser, nil
+	}
+
+	return nil, ErrUserNotFound
+}
+
+func (u *UserStore) Update(user User) (*User, error) {
+	if _, ok := u.items[user.ID]; !ok {
+		return nil, ErrUserNotFound
+	}
+
+	u.items[user.ID] = user
+	localUser := u.items[user.ID]
+
+	return &localUser, nil
+}
+
+func (u *UserStore) Delete(id uuid.UUID) error {
+	delete(u.items, id)
+	return nil
 }
---
+++ internal/stores/users_test.go
@@ -0,0 +1,70 @@
+package stores
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"testing"
+)
+
+func TestUserStore(t *testing.T) {
+	userStore := NewUserStore()
+	user := User{
+		ID:             uuid.New(),
+		Email:          "some@email.com",
+		EmailConfirmed: nil,
+		PasswordHash:   "password",
+	}
+
+	u, err := userStore.Create(user)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if u.ID != user.ID {
+		t.Fatalf("id doesn't match. expected: %v. got: %v", user.ID, u.ID)
+	}
+
+	u2, err := userStore.Get(user.ID)
+	if err != nil {
+		t.Fatalf("unexpected Get error on existing user: %s", err)
+	}
+
+	if u2.ID != user.ID {
+		t.Fatalf("Get: IDs don't match. expected: %v. got: %v", user.ID, u2.ID)
+	}
+
+	wrongID := uuid.New()
+	_, err = userStore.Get(wrongID)
+	if !errors.Is(err, ErrUserNotFound) {
+		t.Fatalf("Get wrong id. unexpected error: %v", err)
+	}
+
+	updatedUser := User{
+		ID:             user.ID,
+		Email:          "new@email.com",
+		EmailConfirmed: nil,
+		PasswordHash:   "password",
+	}
+	u3, err := userStore.Update(updatedUser)
+	if err != nil {
+		t.Fatalf("update: unexpected error: %s", err)
+	}
+
+	if u3.Email != updatedUser.Email {
+		t.Fatalf("value not updated. expected: %s. got: %s", updatedUser.Email, u3.Email)
+	}
+
+	err = userStore.Delete(user.ID)
+	if err != nil {
+		t.Fatalf("delete: unexpected error: %s", err)
+	}
+
+	u4, err := userStore.Get(user.ID)
+	if !errors.Is(err, ErrUserNotFound) {
+		t.Fatalf("get after delete: unexpected error: %v", err)
+	}
+
+	if u4 != nil {
+		t.Fatalf("get after delete: user should be nil, got: %v", u4)
+	}
+}
